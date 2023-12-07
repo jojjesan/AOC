95dc8d1767e31474bab032d58f27658269ce80e4
Jojje Sandholm <georg.sandholm@xlent.se>
2023-12-07 16:51:22 +0100

    Day 6 and 7

---
+++ 2023/Day6/Program.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Immutable;
+using System.Data;
+using System.Xml;
+
+namespace Day6
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            bool test = false;
+            string data = "";
+            if (!test)
+            {
+                string sessionKey = File.ReadAllText("C:\\Source\\Aoc\\sessionkey.txt");
+                var client = new HttpClient();
+                client.DefaultRequestHeaders.Add("Cookie", $"session={sessionKey}");
+                data = await client.GetStringAsync("https://adventofcode.com/2023/day/6/input");
+            }
+            else
+            {
+                Console.WriteLine("## RUNNING TEST DATA");
+                data = testData.Replace("\r", "");
+            }
+
+            // Parse indata
+            var lines = data.Split("\n").ToArray();
+            var raceDurations = lines[0].Split(": ")[1].Split(" ").Where(x => x.Trim() != "")
+                .Select(x => int.Parse(x.Trim())).ToArray();
+            var recordDistances = lines[1].Split(": ")[1].Split(" ").Where(x => x.Trim() != "")
+                .Select(x => int.Parse(x.Trim())).ToArray();
+
+            var res = 1;
+            for (var i = 0; i < raceDurations.Length; i++)
+            {
+                var holdTime = 0;
+                var ix = 0;
+                while (holdTime < raceDurations[i])
+                {
+                    if (GetDistance(holdTime, raceDurations[i]) > recordDistances[i])
+                        ix++;
+                    holdTime++;
+                }
+                Console.WriteLine($"Wins: {ix}");
+                res = res * ix;
+            }
+
+            Console.WriteLine($"Part 1: {res}");
+
+            // Part 2
+
+            var raceDuration = long.Parse(lines[0].Split(":")[1].Replace(" ", ""));
+            var recordDistance = long.Parse(lines[1].Split(":")[1].Replace(" ", ""));
+
+            Console.WriteLine($"Part 2: Duration={raceDuration} Record={recordDistance}");
+            var roots = GetRoots(-raceDuration, recordDistance);
+            Console.WriteLine($"Part 2: Root 1={roots.Item1} Root 2={roots.Item2}");
+
+            var firstRecord = (long)Math.Floor(roots.Item1);
+            var lastRecord = (long)Math.Ceiling(roots.Item2);
+
+            Console.WriteLine($"Part 2: First start={GetDistance(firstRecord, raceDuration)} Last start={GetDistance(lastRecord, raceDuration)}");
+            while (GetDistance(firstRecord, raceDuration) <= recordDistance)
+                firstRecord++;
+            while (GetDistance(lastRecord, raceDuration) <= recordDistance)
+                lastRecord--;
+            Console.WriteLine($"Part 2: First end={GetDistance(firstRecord, raceDuration)} Last end={GetDistance(lastRecord, raceDuration)}");
+
+            Console.WriteLine($"Part 2: Solution={lastRecord-firstRecord+1}");
+
+            // h * (r-h) = rd
+            // h*r - h*h = rd
+            // h*h - r*h + rd = 0
+
+        }
+
+        static Tuple<double, double> GetRoots(double p, double q)
+        {
+            var root = Math.Sqrt((p/2)*(p/2) - q);
+            return Tuple.Create(-p/2 - root, -p / 2 + root);
+        }
+
+        static long GetDistance(long holdTime, long raceDuration)
+        {
+            return holdTime * (raceDuration - holdTime);
+        }
+
+
+        private static string testData = @"Time:      7  15   30
+Distance:  9  40  200";
+    }
+
+    
+}
---
+++ 2023/Day7/Program.cs
@@ -0,0 +1,247 @@
+﻿using System.Collections.Immutable;
+using System.Data;
+using System.Reflection.Metadata;
+using System.Xml;
+
+namespace Day7
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            bool test = false;
+            string data = "";
+            if (!test)
+            {
+                string sessionKey = File.ReadAllText("C:\\Source\\Aoc\\sessionkey.txt");
+                var client = new HttpClient();
+                client.DefaultRequestHeaders.Add("Cookie", $"session={sessionKey}");
+                data = await client.GetStringAsync("https://adventofcode.com/2023/day/7/input");
+            }
+            else
+            {
+                Console.WriteLine("## RUNNING TEST DATA");
+                data = testData.Replace("\r", "");
+            }
+
+            // Parse indata
+            var lines = data.Split("\n").Where(x => x != "").ToArray();
+            var hands = new List<Hand>();
+            foreach (var line in lines)
+            {
+                var hand = new Hand(line.Split(" ")[0].Select(x => GetCardInt(x)).ToList());
+                hand.Bid = int.Parse(line.Split(" ")[1]);
+                hand.Score = GetScore(hand.Cards);
+                hands.Add(hand);
+            }
+
+            // Part 1
+            //Part1(hands);
+
+            // Part 2
+            Part2(hands);
+            // 255488666: too low
+            // 255632664
+
+        }
+        static void Part2(List<Hand> hands)
+        {
+            foreach (var hand in hands)
+            {
+                // J is joker and ranks last
+                hand.Cards = hand.Cards.Select(x => x == 11 ? 1 : x).ToList();
+                hand.Score = GetScore2(hand.Cards);
+
+            }
+            hands.Sort((x, y) => CompareHands(x, y));
+
+            Console.WriteLine($"Hands: {string.Join("\n", hands.Select(x => x.ToString()).ToArray())}");
+
+            var sum = 0;
+            for (var i = 0; i < hands.Count; i++)
+            {
+                sum += hands[i].Bid * (i + 1);
+            }
+
+            Console.WriteLine($"Part 2: {sum}");
+
+        }
+
+        static void Part1(List<Hand> hands)
+        {
+            hands.Sort((x, y) => CompareHands(x, y));
+
+            Console.WriteLine($"Hands: {string.Join("\n", hands.Select(x => x.ToString()).ToArray())}");
+
+            var sum = 0;
+            for (var i = 0; i < hands.Count; i++)
+            {
+                sum += hands[i].Bid * (i + 1);
+            }
+
+            Console.WriteLine($"Part 1: {sum}");
+
+        }
+
+        static int CompareHands(Hand x, Hand y)
+        {
+            if (x.Score > y.Score)
+                return 1;
+            if (x.Score < y.Score)
+                return -1;
+            for (var i = 0; i < x.Cards.Count; i++)
+            {
+                if (x.Cards[i] > y.Cards[i])
+                    return 1;
+                if (x.Cards[i] < y.Cards[i])
+                    return -1;
+            }
+            return 0;
+        }
+
+        static int GetScore2(List<int> cards)
+        {
+            var cardDict = new Dictionary<int, int>();
+            var nbrOfJokers = cards.Where(x => x == 1).Count();
+            if (nbrOfJokers >= 4)
+            {
+                return 6;
+            }
+
+            foreach (var card in cards)
+            {
+                if (card == 1) continue;
+
+                if (cardDict.ContainsKey(card))
+                    cardDict[card]++;
+                else
+                    cardDict.Add(card, 1);
+            }
+
+            // 3, 4 or 5 jokers will result in 4 or five of the same card
+            if (cardDict.Values.Max() + nbrOfJokers == 5)
+            {
+                return 6;
+            }
+            if (cardDict.Values.Max() + nbrOfJokers == 4)
+            {
+                return 5;
+            }
+
+            // The rest of the cases can have maximum 2 jokers
+            if (cardDict.Values.Max() == 3 && cardDict.Values.Min() == 2)
+            {
+                return 4;
+            }
+            if (cardDict.Values.Max() == 3)
+            {
+                // 1 or 2 jokers will behandled earlier (4 or 5 of a kind)
+                return 3;
+            }
+            if (cardDict.Values.Where(x => x == 2).Count() == 2)
+            {
+                if (nbrOfJokers == 1)
+                {
+                    return 4;
+                }
+                return 2;
+            }
+            if (cardDict.Values.Where(x => x == 2).Count() == 1)
+            {
+                // 2 or 3 jokers will behandled earlier (4 or 5 of a kind)
+                if (nbrOfJokers == 1)
+                {
+                    return 3;
+                }
+                return 1;
+            }
+            if (nbrOfJokers == 2)
+            {
+                return 3;
+            }
+            if (nbrOfJokers == 1)
+            {
+                return 1;
+            }
+            return 0;
+        }
+
+        static int GetScore(List<int> cards)
+        {
+            var cardDict = new Dictionary<int, int>();
+            foreach (var card in cards)
+            {
+                if (cardDict.ContainsKey(card))
+                    cardDict[card]++;
+                else
+                    cardDict.Add(card, 1);
+            }
+            if (cardDict.Values.Max() == 5)
+            {
+                return 6;
+            }
+            if (cardDict.Values.Max() == 4)
+            {
+                return 5;
+            }
+            if (cardDict.Values.Max() == 3 && cardDict.Values.Min() == 2)
+            {
+                return 4;
+            }
+            if (cardDict.Values.Max() == 3)
+            {
+                return 3;
+            }
+            if (cardDict.Values.Where(x => x == 2).Count() == 2)
+            {
+                return 2;
+            }
+            if (cardDict.Values.Where(x => x == 2).Count() == 1)
+            {
+                return 1;
+            }
+            return 0;
+        }
+
+        static int GetCardInt(char card)
+        {
+            switch (card)
+            {
+                case 'T':
+                    return 10;
+                case 'J':
+                    return 11;
+                case 'Q':
+                    return 12;
+                case 'K':
+                    return 13;
+                case 'A':
+                    return 14;
+                default:
+                    return int.Parse(card.ToString());
+            }
+        }
+
+        private static string testData = @"32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483";
+    }
+    public class Hand
+    {
+        public List<int> Cards { get; set; }
+        public int Score { get; set; }
+        public int Bid { get; set; }
+        public Hand(List<int> cards)
+        {
+            Cards = cards;
+        }
+
+        public string ToString()
+        {
+            return $"{string.Join(" ", Cards.Select(x => x.ToString()).ToArray())} Score={Score} Bid={Bid}";
+        }
+    }
+
+}
